5543d25bd755239f7f455b732a7b70972246c2b8
skok1025 <skok1025@naver.com>
2019-04-25 16:33:20 +0900

    init commit

---
+++ hello2.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+int main()
+{
+	char* str ="Hello World\n";
+	write(1,str, strlen(str));
+
+	return 0;
+}
+
---
+++ hello4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	const char* str ="Hello World\n";
+
+	FILE* fp = fopen("./hello4.txt", "w");	
+	fputs(str, fp);
+	fclose(fp);
+
+	return 0;
+}
+
